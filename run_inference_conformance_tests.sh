169716b16cb77d531c4d0c970e03fe2386d4d3f8
Dawid Nowak <nowakd@gmail.com>
2025-08-06 21:17:46 +0100

    Creating release 0.1.1

--- run_inference_conformance_tests.sh
+++ run_inference_conformance_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-go test -v -count=1 -timeout=3h ./conformance --debug -run TestConformance --report-output="../kubvernor-inference-conformance-output-1.2.1.yaml" --organization=kubvernor --project=kubvernor --url=https://github.com/kubvernor/kubvernor --version=0.1.0  --contact=nowakd@gmail.com --allow-crds-mismatch
+go test -v -count=1 -timeout=3h ./conformance --debug -run TestConformance --report-output="../kubvernor-inference-conformance-output-0.1.1.yaml" --organization=kubvernor --project=kubvernor --url=https://github.com/kubvernor/kubvernor --version=0.1.1  --contact=nowakd@gmail.com --allow-crds-mismatch
 cd ..
